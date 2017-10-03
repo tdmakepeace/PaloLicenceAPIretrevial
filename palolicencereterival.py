d18041d64a3ee407656c430cbc3b3e62e4cbd99b
Toby Makepeace <tdmakepeace@gmail.com>
2017-10-03 15:58:46 +0100

    Add files via upload

--- palolicencereterival.py
+++ palolicencereterival.py
@@ -85,32 +85,84 @@ def serialregister():
 
     for x in resp_str:
 # Count the number of licences in the file.
-        c = x.count('partidField')
-        resp = json.loads(x)
-        
-        serial = resp[0]['lfidField']
-        print  "your serial number is  = '%s'" %serial
-        print "your licence keys start with your serial number and then the function of the Key."
-
-# for every licence create a file with the output.
-# use the serial number and the licence type. 
-#
-        i=0
-        while i < c:
-            fName = serial+"-"+resp[i]['partidField']+".key"
-#Uncomment for debug
-#            print fName
-#            print (resp[i]['keyField'])
-
-            file = open(fName,"w") 
-            file.write(resp[i]['keyField'])
-            file.close() 
-            i+=1
-        
+        lic = x.count('partidField')
+        print  "you have '%s' licences" %lic
+ 
     resp_str.close()
+
+# map the authcode into the data field.
+    data = "authcode=%s" %authcode
+    url = 'https://api.paloaltonetworks.com/api/license/get'
+    headers = { 'apikey':api , 'Content-Type':'application/x-www-form-urlencoded' }
+    req = urllib2.Request(url, data , headers)
+    resp_str1 = urllib2.urlopen(req)
+
+    for a in resp_str1:
+
+        resp = json.loads(a)   
+
+
+        d = resp['UsedCount']
+        print  "the number of devices  = '%s'" %d
+
+        print(resp)
+
+        cpuid = raw_input("What is the cpuid of the device you wish to register:")
+
+
+        ## for every licence create a file with the output.
+        ## use the serial number and the licence type. 
+        ##
+        x=0
+        while x < d:
+            print (resp['UsedDeviceDetails'][x]['SerialNumber'])+","+(resp['UsedDeviceDetails'][x]['CPUID'])+","+(resp['UsedDeviceDetails'][x]['UUID'])
+            if cpuid == (resp['UsedDeviceDetails'][x]['CPUID']):
+                serial = (resp['UsedDeviceDetails'][x]['SerialNumber'])
+                data = "serialNumber="+serial
+
+            #   debug
+            #   print data
+
+                url = 'https://api.paloaltonetworks.com/api/license/activate'
+                headers = { 'apikey':api , 'Content-Type':'application/x-www-form-urlencoded' }
+                req = urllib2.Request(url, data , headers)
+                resp_str2 = urllib2.urlopen(req)
+
+                for y in resp_str2:
+            # Count the number of licences in the file.
+                    c = y.count('partidField')
+                    resp = json.loads(y)
+
+            # for every licence create a file with the output.
+            # use the serial number and the licence type. 
+            #
+                    i=0
+                    while i < c:
+                        fName = serial+"-"+resp[i]['partidField']+".key"
+            #Uncomment for debug
+            #            print fName
+            #            print (resp[i]['keyField'])
+
+                        file = open(fName,"w") 
+                        file.write(resp[i]['keyField'])
+                        file.close() 
+                        i+=1
+
+                resp_str2.close()
+
+            else:
+                print "nothing found"
+
+            x=x+1    
+
+        print "Finished"
+    resp_str1.close()
+
 
     
 def registeruser():
+# not working as i need a CSSP account to work with.
+
     
     SERIAL = raw_input("The following details as required to be registered against a device \nWhat is the Serial number of the device you wish to register details against \n:")
     CustomerAccountId = raw_input("Unique Reference number, used for billing tracking:")
@@ -152,6 +204,92 @@ def registeruser():
     result = resp_str.read()
     print result
 
+
+def authcode():
+    
+    
+    authcode = raw_input("What is the authcode you wish to validate:")
+    data = "authcode=%s" %authcode
+
+    url = 'https://api.paloaltonetworks.com/api/license/get'
+    headers = { 'apikey':api , 'Content-Type':'application/x-www-form-urlencoded' }
+    req = urllib2.Request(url, data , headers)
+    resp_str1 = urllib2.urlopen(req)
+
+    for a in resp_str1:
+
+        resp = json.loads(a)   
+
+        d = resp['UsedCount']
+        print  "the number of devices  = '%s'" %d
+
+        print(resp)
+
+
+        x=0
+        while x < d:
+            print (resp['UsedDeviceDetails'][x]['SerialNumber'])+","+(resp['UsedDeviceDetails'][x]['CPUID'])+","+(resp['UsedDeviceDetails'][x]['UUID'])
+            licence = raw_input("Do you need me to generate licences, y or n :")
+    
+            if licence == 'y':
+                serial = (resp['UsedDeviceDetails'][x]['SerialNumber'])
+                data = "serialNumber="+serial
+
+            #   debug
+            #   print data
+
+                url = 'https://api.paloaltonetworks.com/api/license/activate'
+                headers = { 'apikey':api , 'Content-Type':'application/x-www-form-urlencoded' }
+                req = urllib2.Request(url, data , headers)
+                resp_str2 = urllib2.urlopen(req)
+
+                for y in resp_str2:
+            # Count the number of licences in the file.
+                    c = y.count('partidField')
+                    resp = json.loads(y)
+
+            # for every licence create a file with the output.
+            # use the serial number and the licence type. 
+            #
+                    i=0
+                    while i < c:
+                        fName = serial+"-"+resp[i]['partidField']+".key"
+            #Uncomment for debug
+            #            print fName
+            #            print (resp[i]['keyField'])
+
+                        file = open(fName,"w") 
+                        file.write(resp[i]['keyField'])
+                        file.close() 
+                        i+=1
+
+                resp_str2.close()
+
+            else:
+                print "nothing found"
+
+            x=x+1    
+
+        print "Finished"
+    resp_str1.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    resp_str1.close()
+
        
 def help(): 	
     print "help menu"  
@@ -171,8 +309,10 @@ if __name__ == '__main__':
         serialregister()
     elif arg == "user":
         registeruser()
+    elif arg == "authcode":    
+        authcode()
     else:
-        print " The options available are help,setup,serial,register,user"
+        print " The options available are help,setup,serial,register,user,authcode"
         print " e.g. 'palolicencereterival.py help'"
         print " or 'palolicencereterival.py serial'"
        
